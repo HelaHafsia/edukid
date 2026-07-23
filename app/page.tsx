628ec47365885e9c32abe174db3dce74ed68bbea
Hela Hafsia <hela.hafsia@keyrus.com>
2026-07-23 17:48:59 +0200

    Trigger redeploy with the right folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function HomePage() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>EduKid</h1>
-      <p>L&apos;application est en cours de construction.</p>
-    </main>
-  );
+  redirect("/connexion");
 }
